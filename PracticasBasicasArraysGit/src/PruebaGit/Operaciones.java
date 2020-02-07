5031342caac23555aa30412384325a6ec688d3ca
Nacho <nachogp17@gmail.com>
2020-02-07 19:18:51 +0100

    Agregada la clase Operaciones

---
+++ PracticasBasicasArraysGit/src/PruebaGit/Operaciones.java
@@ -0,0 +1,30 @@
+package PruebaGit;
+
+public class Operaciones {
+	
+	private int OperadorA;
+	private int OperadorB;
+	
+	public Operaciones(int OperadorA, int OperadorB) {
+		super();
+		this.OperadorA = OperadorA;
+		this.OperadorB = OperadorB;
+	}
+	
+	public int getOperadorA() {
+		return OperadorA;
+	}
+	public void setOperadorA(int operadorA) {
+		OperadorA = operadorA;
+	}
+	public int getOperadorB() {
+		return OperadorB;
+	}
+	public void setOperadorB(int operadorB) {
+		OperadorB = operadorB;
+	}
+	
+	
+	
+
+}
